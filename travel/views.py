6a26babfc4aeff42548b911d07853f3022f00225
Игорь <pupik.1985@list.ru>
2022-01-14 14:39:19 +1000

    new changes

--- travel/views.py
+++ travel/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 def home(request):
     name= "Harry"
     return render(request, 'home.html', {'name':name})
+
+
+def about(request):
+    return render(request, 'about.html')
