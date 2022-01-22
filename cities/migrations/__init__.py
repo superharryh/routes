a28d72b8ad4b4aa614972b964f51ef2191dcd998
Игорь <pupik.1985@list.ru>
2022-01-23 02:40:58 +1000

    ghh

--- cities/migrations/__init__.py
+++
@@ -1,3 +0,0 @@
-from django.core.serializers import register_serializer
-
-register_serializer('json-no-uescape', 'serializers.json_no_uescape')
--- cities/serializers/json_no_uescape.py
+++
@@ -1,8 +0,0 @@
-from django.core.serializers.json import *
-
-
-class Serializer(Serializer):
-
-    def _init_options(self):
-        super(Serializer, self)._init_options()
-        self.json_kwargs['ensure_ascii'] = False
